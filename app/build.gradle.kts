c10b7f64afba36db243a2bb0bb3d2e5379767391
a0527198150-del <a0527198150@gmail.com>
2026-07-09 17:32:45 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.kosherchannels.yfqbzw"
+    applicationId = "com.aistudio.kosherchannels.yfqbzw2"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
